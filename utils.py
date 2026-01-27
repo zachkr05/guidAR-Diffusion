fa57f338a76b79b6cf14c1d4bd44116cbb439bce
zachkr05 <zachkr05@gmail.com>
2026-01-26 21:56:12 -0500

    Added draggable spline

--- utils.py
+++ utils.py
@@ -11,104 +11,8 @@ from matplotlib import cm
 import numpy as np
 from skimage.graph import route_through_array
 from scipy.interpolate import BSpline
+from spline import *
 
-
-def find_span(n, p, u, U):
-    """
-    Find span index for u.
-    n = n_ctrl - 1 (highest control index)
-    """
-    # Special case at the end
-    if u >= U[n+1]:
-        return n
-    if u <= U[p]:
-        return p
-
-    low, high = p, n+1
-    mid = (low + high) // 2
-    while u < U[mid] or u >= U[mid+1]:
-        if u < U[mid]:
-            high = mid
-        else:
-            low = mid
-        mid = (low + high) // 2
-    return mid
-
-def basis_funs(span, u, p, U):
-    """Compute the nonzero B-spline basis functions N_{span-p...span,p}(u)."""
-    N = np.zeros(p+1, dtype=float)
-    left = np.zeros(p+1, dtype=float)
-    right = np.zeros(p+1, dtype=float)
-    N[0] = 1.0
-
-    for j in range(1, p+1):
-        left[j] = u - U[span+1-j]
-        right[j] = U[span+j] - u
-        saved = 0.0
-        for r in range(j):
-            denom = right[r+1] + left[j-r]
-            # denom should be > 0 for valid knot vectors, but guard anyway
-            temp = 0.0 if denom == 0 else N[r] / denom
-            N[r] = saved + right[r+1] * temp
-            saved = left[j-r] * temp
-        N[j] = saved
-    return N
-
-
-def bspline_design_matrix(u, n_ctrl, degree, U):
-    p = degree
-    n = n_ctrl - 1
-    A = np.zeros((len(u), n_ctrl), dtype=float)
-
-    for j, uj in enumerate(u):
-        span = find_span(n, p, uj, U)
-        N = basis_funs(span, uj, p, U)  # length p+1
-        i0 = span - p
-        A[j, i0:i0+p+1] = N
-
-    return A
-
-def clamped_knots(num_ctrl_pts, degree):
-    p = degree
-    m = num_ctrl_pts + p 
-    U = np.zeros(m+1, dtype=float)
-
-    U[:p+1] = 0.0
-    U[m-p:] = 1.0
-
-    num_interior = num_ctrl_pts - p - 1
-    U[p+1:m-p] = np.linspace(0.0, 1.0, num_interior + 2)[1:-1]
-
-    return U
-
-def reparam_curve(Q):
-    d = np.linalg.norm(Q[1:] - Q[:-1], axis=1)
-    total = d.sum()
-    u = np.concatenate(([0.0], np.cumsum(d) / total))
-    u[-1] = 1.0
-    return u
-
-def generate_clamped_spline(x_np, y_np, k, num_ctrl_pts):
-    Q = np.column_stack([x_np, y_np])
-    u = reparam_curve(Q)
-    U = clamped_knots(num_ctrl_pts, k)
-    A = bspline_design_matrix(u, num_ctrl_pts, k, U)
-    
-    P, *_ = np.linalg.lstsq(A,Q,rcond=None)
-    P[0]  = Q[0]
-    P[-1] = Q[-1]
-    P = np.asarray(P)
-
-    t0, t1 = U[k], U[-k-1]
-    t = np.linspace(t0, t1, 500)
-
-    splx = BSpline(U, P[:, 0], k)
-    sply = BSpline(U, P[:, 1], k)
-
-    x_s = splx(t)
-    y_s = sply(t)
-
-    return x_s, y_s, P
 
 def get_user_adjustments(fused_costmap, obstacle_positions, radii, goal_position):
 
@@ -123,16 +27,17 @@ def get_user_adjustments(fused_costmap, obstacle_positions, radii, goal_position
     #https://stackoverflow.com/questions/32551536/draw-marker-in-image
     #plt.annotate('25, 50', xy=(25, 50))
     #print(obstacle_positions)
-    
+   
+    fig, ax = plt.subplots()
     colors = {'chair': 'green', 'table': 'red', 'bomb': 'blue'}
 
     #Tech debt but obstacle positions is a list of dictionaries
     for cls, obs_list in obstacle_positions[0].items():
         for pos in obs_list:
-            plt.plot(pos[1],pos[0], color=colors[cls], marker='o', label=f'{cls}')
+            ax.plot(pos[1],pos[0], color=colors[cls], marker='o', label=f'{cls}')
     goal = goal_position[0]
 
-    plt.plot(goal[1], goal[0], color='olive', marker='*', label='goal')
+    ax.plot(goal[1], goal[0], color='olive', marker='*', label='goal')
 
     #Get path
     path = route_through_array(map_np, [0,0], (goal[0],goal[1]), fully_connected=True)
@@ -142,15 +47,17 @@ def get_user_adjustments(fused_costmap, obstacle_positions, radii, goal_position
 
     k = 3
     num_ctrl_pts = 10
-    x_s, y_s, P = generate_clamped_spline(x_np, y_np, k, num_ctrl_pts)
+    x_s, y_s, P, U= generate_clamped_spline(x_np, y_np, k, num_ctrl_pts)
 
     #Plot B-Spline and control points
-    plt.plot(x_s, y_s, linewidth=2, label="B-spline curve")
-    plt.plot(P[:, 0], P[:, 1], "o--", alpha=0.7, label="control polygon")
+    ax.plot(x_s, y_s, linewidth=2, label="Original B-spline curve")
+    ax.plot(P[:, 0], P[:, 1], "o--", alpha=0.7, label="original control polygon")
     
-    plt.imshow(map_np, origin='lower')
-    plt.colorbar()
-    plt.legend(loc='best')
+    dragger = DraggableBSpline(ax, U, P, k, n_samples=800)
+
+    ax.imshow(map_np, origin='lower')
+    #plt.colorbar()
+    ax.legend(loc='best')
     plt.show()
 
 def visualize_3d(fused_cm):
